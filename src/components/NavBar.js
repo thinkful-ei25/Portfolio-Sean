5e94e522a4f5d242ea1d8407dd4eb92f6a509eaf
Sean Nealon <signalflowsean@gmail.com>
2019-02-05 17:55:32 -0500

    added social media icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import { logo, home, about, projects, contact } from '../images/nav/index.images.nav'; 
+import { linkedin, github, twitter } from '../images/socialMedia/index.images.socialMedia'; 
 import '../styles/nav.css'; 
 
 const NavBar = () => (
@@ -8,12 +9,19 @@ const NavBar = () => (
       <img className='nav-button' src={logo} alt='logo' />
       <p className='brand-name'>signalflowsean</p>
     </div>
-    <div className='nav-container'> 
+    <nav className='nav-container'> 
       <img className='nav-button' src={home} alt='home' />
       <img className='nav-button' src={about} alt='about' />
       <img className='nav-button' src={projects} alt='projects' />
       <img className='nav-button' src={contact} alt='contact' />
-    </div>
+    </nav>
+    <ul className='social-media-list'>
+      <li className='social-media-list-item'>
+        <img className='nav-button' src={linkedin} alt='linkedin' />
+        <img className='nav-button' src={github} alt='github' />
+        <img className='nav-button' src={twitter} alt='twitter' />
+      </li>
+    </ul>
     
   </section>
 );
