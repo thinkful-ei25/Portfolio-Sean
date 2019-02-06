f20945c2ec95d9043bbc59657f8d62d078c9c6df
Sean Nealon <signalflowsean@gmail.com>
2019-02-06 17:53:45 -0500

    added anchor tags to about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,8 +13,9 @@ const About = () => (
       their innate musical <em>creativity</em> and learn about music in <b className='fun'>fun </b><br></br> 
       <p className='interactive'><b>i</b><b>n</b><b>t</b><b>e</b><b>r</b><b>a</b><b>c</b><b>t</b><b>i</b><b>v</b><b>e</b></p> ways. </p>
       <p className='about-text second-blurb'>He has worked at a plethora of musical software companies some of which include: 
-      Harmonix, Qleek, and Point Motion. Recently, he graduated from a web development bootcamp called 
-      Thinkful and is looking for ways to continue developing his programming skills. </p>
+      <a href='http://www.harmonixmusic.com/'> Harmonix</a>, <a href='https://getqleek.com/#scroll-1526577506740'>Qleek</a>, and 
+      <a href='http://www.pointmotioncontrol.com/'>Point Motion</a>. Recently, he graduated from a web development bootcamp called Thinkful and is looking for ways to 
+      continue developing his programming skills. </p>
       <img className='about-img' src={aboutImg} alt='about me'></img>
     </div>    
   </section>
