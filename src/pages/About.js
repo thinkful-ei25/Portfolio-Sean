c312b69f1ea91ec41ae2e881bd6b12522fdcc9f8
Sean Nealon <signalflowsean@gmail.com>
2019-02-06 13:22:22 -0500

    added text for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,14 @@ import '../styles/about.css';
 
 const About = () => (
   <section className='about-container'>
-    <img className='about-img' src={aboutImg} alt='about me'></img>
+    <div> 
+      <p className='about-text first-blurb'>Sean strives to make innovative musical software that allows users to express 
+      their innate musical creativity and learn about music in fun interactive ways. </p>
+      <p className='about-text second-blurb'>He has worked on a plethora of interactive musical systems including: DropMix at Harmonix, Qleek, Point Motion. 
+      Recently, he graduated a web development bootcamp called Thinkful and is looking for ways to continue 
+      developing his programming skills. </p>
+      <img className='about-img' src={aboutImg} alt='about me'></img>
+    </div>    
   </section>
 ); 
 
