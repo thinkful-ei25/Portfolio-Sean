c52c657ba96d21c76f802701cf69ede364b11a1c
Sean Nealon <signalflowsean@gmail.com>
2019-02-06 13:33:22 -0500

    added header

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ import '../styles/about.css';
 const About = () => (
   <section className='about-container'>
     <div> 
+      <h2 className='about-text about-header'>ABOUT</h2>
       <p className='about-text first-blurb'>Sean strives to make innovative musical software that allows users to express 
       their innate musical creativity and learn about music in fun interactive ways. </p>
       <p className='about-text second-blurb'>He has worked on a plethora of interactive musical systems including: DropMix at Harmonix, Qleek, Point Motion. 
