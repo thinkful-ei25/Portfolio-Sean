bae145425c261c070d80ff49686f69a6c0dca5fe
Sean Nealon <signalflowsean@gmail.com>
2019-02-06 10:55:30 -0500

    added all the tags

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,6 +6,10 @@ const LandingPage = () => (
   <div className='landing-container'>
     <h2 className='landing-text landing-brand-name'>signalflowsean</h2>
     <h2 className='landing-text landing-occupation-name'>full stack web developer</h2>
+    <h3 className='landing-text landing-musician-tag'>musician</h3>
+    <h3 className='landing-text landing-interactive-tag'>interactive systems designer</h3>
+    <h3 className='landing-text landing-game-tag'>unity game dev</h3>
+    <h3 className='landing-text landing-maxmsp-tag'>max msp guru</h3>
     <img src={landing} 
       className='landing-image' 
       alt='landing' >
