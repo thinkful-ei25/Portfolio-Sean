dc236334b89e0d1d4fe1fdb25949f251ef1fda58
Sean Nealon <signalflowsean@gmail.com>
2019-02-06 14:40:54 -0500

    added fun css

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,14 @@ import React from 'react';
 import aboutImg from '../images/pages/aboutImg.jpg'; 
 import '../styles/about.css'; 
 import '../creative.scss'; 
+import '../fun.scss'; 
 
 const About = () => (
   <section className='about-container'>
     <div> 
       <h2 className='about-text about-header'>ABOUT</h2>
       <p className='about-text first-blurb creative'>Sean strives to make innovative musical software that allows users to express 
-      their innate musical <em>creativity</em> and learn about music in fun interactive ways. </p>
+      their innate musical <em>creativity</em> and learn about music in <b className='fun'>fun</b> interactive ways. </p>
       <p className='about-text second-blurb'>He has worked on a plethora of interactive musical systems including: DropMix at Harmonix, Qleek, and at Point Motion. 
       Recently, he graduated a web development bootcamp called Thinkful and is looking for ways to continue 
       developing his programming skills. </p>
