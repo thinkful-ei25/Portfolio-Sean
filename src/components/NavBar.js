b53824edf9083a3ebbb07314a0d73f7a42eb3888
Sean Nealon <signalflowsean@gmail.com>
2019-02-08 12:34:13 -0500

    scrolling works

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react'; 
-import { slide as Menu } from 'react-burger-menu'
+import { slide as Menu } from 'react-burger-menu'; 
+import * as Scroll from 'react-scroll'; 
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'; 
 import { logo, home, about, projects, contact, linkedin, github, twitter, hamburger } 
   from '../images/nav/index.images.nav'; 
 import '../styles/nav.css'; 
@@ -7,7 +9,58 @@ import '../styles/nav.css';
 const navImages = [home, about, projects]; 
 const socialMediaImages = [linkedin, github, twitter];
 
+// let Link       = Scroll.Link;
+// let Element    = Scroll.Element;
+// let Events     = Scroll.Events;
+// let scroll     = Scroll.animateScroll;
+// let scrollSpy  = Scroll.scrollSpy;
+ 
+// // ES5
+// var React   = require('react');
+// var Scroll  = require('react-scroll');
+ 
+// var Link       = Scroll.Link;
+// var Element    = Scroll.Element;
+// var Events     = Scroll.Events;
+// var scroll     = Scroll.animateScroll;
+// var scrollSpy  = Scroll.scrollSpy;
+
 export default class NavBar extends React.Component {
+  componentDidMount = () => {
+ 
+    Events.scrollEvent.register('begin', function(to, element) {
+      console.log("begin", arguments);
+    });
+ 
+    Events.scrollEvent.register('end', function(to, element) {
+      console.log("end", arguments);
+    });
+ 
+    scrollSpy.update();
+ 
+  }
+  componentWillUnmount = () => { 
+
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+  scrollToTop = () => { 
+    console.log('hi')
+    scroll.scrollToTop();
+  }
+  scrollToBottom = () => {
+    scroll.scrollToBottom();
+  }
+  scrollTo = () => {
+    scroll.scrollTo(800);
+  }
+  scrollMore = () => {
+    scroll.scrollMore(100);
+  }
+  handleSetActive= (to) => {
+    console.log(to);
+  }
+
   showSettings (event) {
     event.preventDefault();
   }
@@ -24,11 +77,27 @@ export default class NavBar extends React.Component {
             <p className='brand-name'>signalflowsean</p>
           </div>
           <div className='nav-container'> 
-            {navImages.map((image, i) => (
+          <Link  onClick={this.scrollToTop}>
+            <div className='nav-button-container'>
+              <img className='nav-button' src={home} alt='home' />
+            </div>
+          </Link>
+           <Link onClick={this.scrollTo}>
+              <div className='nav-button-container'>
+                <img className='nav-button' src={about} alt='about' />
+              </div>
+            </Link>
+            <Link onClick={this.scrollToBottom}>
+              <div className='nav-button-container'>
+                <img className='nav-button' src={projects} alt='projects' />
+              </div>
+            </Link>
+            {/* <img className='nav-button' src={contact} alt='contact' /> */}
+            {/* {navImages.map((image, i) => (
               <div key={i} className='nav-button-container'>
                 <img key={i} src={image} className='nav-button' alt='nav-button' />
               </div>
-            ))}
+            ))} */}
           </div>
           <ul className='social-media-list'>
               {socialMediaImages.map((image, i) => (
