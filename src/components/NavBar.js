e1f50463fc93f3265449d3f648c92f02cf4691fd
Sean Nealon <signalflowsean@gmail.com>
2019-02-11 20:17:56 -0500

    desktop position better

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,48 +49,77 @@ export default class NavBar extends React.Component {
 
   render() { 
     return ( 
-      <section className='nav-bar'>
-        <Menu 
-          customBurgerIcon={<img className='hamburger' src={hamburger} alt='hamburger-menu'></img> } 
-          isOpen={true} 
-          id ='nav-bar' 
-          pageWrapId={ "page-wrap" }>
+      <section>
+        <div id='desktop-nav-bar'>
           <div>
-            <img className='logo' src={logo} alt='logo' />
-            <p className='brand-name'>signalflowsean</p>
+            <img className='logo-image' src={logo} alt='logo'></img>
+            <p className='brand-name' >sean</p>
           </div>
-          <div className='nav-container'> 
-          <Link  onClick={this.scrollToTop}>
-            <div className='nav-button-container'>
+          <div className='nav-button-container'>
+            <Link className='nav-link' onClick={this.scrollToTop}>
               <img className='nav-button' src={home} alt='home' />
-            </div>
-          </Link>
-           <Link onClick={this.scrollTo}>
-              <div className='nav-button-container'>
-                <img className='nav-button' src={about} alt='about' />
-              </div>
             </Link>
-            <Link onClick={this.scrollToBottom}>
-              <div className='nav-button-container'>
-                <img className='nav-button' src={projects} alt='projects' />
-              </div>
+            <Link className='nav-link' onClick={this.scrollTo}>
+              <img className='nav-button' src={about} alt='about' />
             </Link>
-            {/* <img className='nav-button' src={contact} alt='contact' /> */}
-            {/* {navImages.map((image, i) => (
-              <div key={i} className='nav-button-container'>
-                <img key={i} src={image} className='nav-button' alt='nav-button' />
-              </div>
-            ))} */}
+            <Link className='nav-link' onClick={this.scrollToBottom}>
+              <img className='nav-button' src={projects} alt='projects' />
+            </Link>
           </div>
-          <ul className='social-media-list'>
+          <div className='social-button-container'>
+            <ul className='social-media-list'>
               {socialMediaImages.map((image, i) => (
                 <li key={i} className='social-media-list-item'>
-                  <img key={i} src={image} className='nav-button' alt='social-media'/>
+                  <img key={i} src={image} className='social-media-button' alt='social-media'/>
                 </li>
-              ))}
-            </ul>
-        </Menu>
+                ))}
+              </ul>
+          </div>
+        </div>
       </section>
+      // <section className='nav-bar'>
+      //   <Menu 
+      //     width={'28px'}
+      //     customBurgerIcon={<img className='hamburger' src={hamburger} alt='hamburger-menu'></img> } 
+      //     isOpen={true} 
+      //     id ='nav-bar'>
+      //     <div>
+      //       <img className='logo' src={logo} alt='logo' />
+      //       <p className='brand-name'>signalflowsean</p>
+      //     </div>
+      //     <div className='nav-container'> 
+      //     <Link  onClick={this.scrollToTop}>
+      //       <div className='nav-button-container'>
+      //         <img className='nav-button' src={home} alt='home' />
+      //       </div>
+      //     </Link>
+      //      <Link onClick={this.scrollTo}>
+      //         <div className='nav-button-container'>
+      //           <img className='nav-button' src={about} alt='about' />
+      //         </div>
+      //       </Link>
+      //       <Link onClick={this.scrollToBottom}>
+      //         <div className='nav-button-container'>
+      //           <img className='nav-button' src={projects} alt='projects' />
+      //         </div>
+      //       </Link>
+      //       {/* <img className='nav-button' src={contact} alt='contact' /> */}
+      //       {/* {navImages.map((image, i) => (
+      //         <div key={i} className='nav-button-container'>
+      //           <img key={i} src={image} className='nav-button' alt='nav-button' />
+      //         </div>
+      //       ))} */}
+      //     </div>
+      //     <ul className='social-media-list'>
+      //         {socialMediaImages.map((image, i) => (
+      //           <li key={i} className='social-media-list-item'>
+      //             <img key={i} src={image} className='nav-button social-media-list-image' alt='social-media 
+      //             '/>
+      //           </li>
+      //         ))}
+      //       </ul>
+      //   </Menu>
+      // </section>
     ); 
   }
 };
