7107316d2369dea0eaed90cd158f5aeba4d48500
Sean Nealon <signalflowsean@gmail.com>
2019-02-12 10:55:28 -0500

    about page is more responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,17 +3,19 @@ import { about, creative, fun, interactive } from '../styles/index.styles'; // e
 
 const About = () => (
   <section className='about-container'>
-    <div> 
-      <p className='about-text first-blurb creative'>Sean strives to make innovative musical software that allows users to express 
-        their innate musical <em>creativity</em> and learn about music in <b className='fun'>fun </b> and 
+    <div className='about-text-container'> 
+      <p>Sean strives to make innovative musical software that allows users to express 
+        their innate musical creativity and learn about music in fun and interactive ways
+        He has worked at a plethora of musical software companies some of which include: 
+        <a href='http://www.harmonixmusic.com/'> Harmonix</a>, 
+        <a href='https://getqleek.com/#scroll-1526577506740'>Qleek</a>, and 
+        <a href='http://www.pointmotioncontrol.com/'> Point Motion</a>. 
+        Recently, he graduated from a web development bootcamp called Thinkful and is looking for ways to 
+        continue developing his programming skills. 
       </p>
-      <p className='interactive first-blurb about-text third-line'> <b>i</b><b>n</b><b>t</b><b>e</b><b>r</b><b>a</b><b>c</b><b>t</b><b>i</b><b>v</b><b>e</b></p> 
-      <p className='about-text first-blurb third-line'>ways.</p>
-      <p className='about-text second-blurb'>He has worked at a plethora of musical software companies some of which include: 
-      <a href='http://www.harmonixmusic.com/'> Harmonix</a>, <a href='https://getqleek.com/#scroll-1526577506740'>Qleek</a>, and 
-      <a href='http://www.pointmotioncontrol.com/'> Point Motion</a>. Recently, he graduated from a web development bootcamp called Thinkful and is looking for ways to 
-      continue developing his programming skills. </p>
-      {/* <img className='about-img' src={aboutImg} alt='about me'></img> */}
+      <p>You can contact him via <a href='signalflowsean@gmail.com'>email</a>, 
+        <a> linkedin</a>, and <a>twitter</a>. 
+      </p>
     </div>    
   </section>
 ); 
