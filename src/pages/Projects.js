47cc121e00ed1c62a4ac26f295e7b73d70fc7fd6
Sean Nealon <signalflowsean@gmail.com>
2019-02-14 11:25:45 -0500

    fixed slect project bug

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -32,11 +32,12 @@ const YouTube = createYouTube();
 const options = [
   { value: 0, label: 'Movements' },
   { value: 1, label: 'Harmonic Space' },
-  { value: 2, label: 'Spine Saver' }, 
-  { value: 3, label: 'BeatFighter'}, 
-  { value: 4, label: 'Universallis'}, 
-  { value: 5, label: 'Teradiddle'}, 
-  { value: 6, label: 'Terpstra'}
+  { value: 2, label: 'Call Meter' },
+  { value: 3, label: 'Spine Saver' }, 
+  { value: 4, label: 'BeatFighter'}, 
+  { value: 5, label: 'Universallis'}, 
+  { value: 6, label: 'Teradiddle'}, 
+  { value: 7, label: 'Terpstra'}
 ];
 export default class Project extends React.Component{ 
     constructor(props) {
--- src/utils/videoURLContainer.js
+++ src/utils/videoURLContainer.js
@@ -30,8 +30,40 @@ const collaborators = [
   'solo', 'George Luther', 'solo'
 ]
 
+const videoIdsInd = 
+  ["mpbdzB28ZBY","r-0IfVQyIEs", "0VVkfkBlTRI", "zWxYKkLlF6E", "mKhk10QJNKk", "i8MTHqo5mU4", "KlKeKQgeHX8", "Z42SIvyWzhM"]; 
+
+  const videoTitlesInd = 
+  ["Movements", "Harmonic Space", "Call Meter","Spine Saver", "BeatFighter","Universallis",  
+  "Teradiddle", "Terpstra"]; 
+
+  const techStackInd = [ 
+    'Max-Msp kinect-v1 samplers', 'max-msp kinect-v2 midi', 'Twilio Node-Mailer Mongo Express React Node', 
+    'TensorFlow Webcam Mongo Express React Node', 'Unity-3d C#', 'Unity C# Max-Msp Midi', 'Unity-3d C#'
+  ]; 
+
+  const liveLinkInd = [{link: false}, {link:false}, {link: true, url:'https://callmeter.netlify.com/', github: 'https://github.com/thinkful-ei25/billable-client'}, 
+  {link: true, url:'https://spine-saver-client-side.herokuapp.com/', github: 'https://github.com/signalflowsean/spineSaver'}, {link: false}, {link: false }, 
+  {link: false }, {link: false }]; 
+
+const videoDescriptionInd = [
+  "In this project, specific hand gestures were used to trigger different sound samples. There were six different libraries of samples each consisting of five samples. Each sample library has a different sonic feel (movement). Certain gestural commands were used with specific samples to amplify the targeted expression. This program was designed to create a specific feeling and meaning through arm and hand gestures without the need for language.",
+  "Our goal in this project was to help people understand the concept of tonal tension and release (consonance and dissonance) through the relationship of space and tonality. A person is placed in a defined area. The program identifies the top five locations the user frequents most as key points. Once five key points have been made, the user can trigger a chord by standing in that spot. Key points trigger specific chords that relate to tonal harmony. Once the points are activated the user can trigger different chords depending on the specific location in real time.", 
+  "CallMeter automates the tedious task of tracking billable time on the phone by creating invoices for clients by automatically tracking minutes spent and programmatically generating invoices unique to each of your clients.",
+  "Having trouble with your posture?! Try Spine Saver. Tracks your slouching metrics and reports back to you your improvement overtime. All you need is a webcam!",
+  "This project allows a user to easily craft a beat and have that beat come to life. The user interacts with the transubstantiated beat by moving it through two dimensional space to destroy their opponent. The player needs to listen to the music to be able to effectively anticipate the other player's moves.  It was made with Unity. ", 
+  "A three dimensional seuquencer, based on the tersptra keyboard layout. There is a twist on this layout as this projects introduces the concept of time by rotating the keyboard continuously, thus a sequencer!",
+  "A musical physics simulation. Bouncing spheres act as triggers to a beautiful harmonic sequencer. Users can control the playback and looping of spheres by interacting with them", 
+  "A symetrical keyboard that is designed to help musicians learn music through geometrical association", 
+]
+
+const collaboratorsInd = [ 
+  'solo', 'Robert Bruce Johnson', 'Brady Fox, Jocelyn Santiago, Allister Rooke, Jonathon Garrett', 'solo', 'solo', 
+  'solo', 'George Luther', 'solo'
+]
+
 export function techStackIndexAt(i){ 
-  return techStack[i]; 
+  return techStackInd[i]; 
 }
 export function techStackRotateLeft(){ 
   return shiftListLeft(techStack); 
@@ -42,7 +74,7 @@ export function techStackRotateRight(){
 }
 
 export function liveLinkAtIndex(i){ 
-  return liveLink[i]; 
+  return liveLinkInd[i]; 
 }
 
 export function videoLinkRotateLeft(){ 
@@ -54,7 +86,7 @@ export function videoLinkRotateRight(){
 }
 
 export function collaboratorsAtIndex(i){ 
-  return collaborators[i]; 
+  return collaboratorsInd[i]; 
 }
 export function videoCollobaratorsRotateLeft(){ 
   return shiftListLeft(collaborators); 
@@ -65,7 +97,7 @@ export function videoCollobaratorsRotateRight() {
 }
 
 export function descriptionAtIndex(i){ 
-  return videoDescription[i]; 
+  return videoDescriptionInd[i]; 
 }
 export function videoDescriptionRotateLeft() { 
   return shiftListLeft(videoDescription); 
@@ -76,7 +108,7 @@ export function videoDescriptionRotateRight() {
 }
 
 export function titlesAtIndex(i){ 
-  return videoTitles[i]; 
+  return videoTitlesInd[i]; 
 }
 
 export function videoTitleRotateLeft() { 
@@ -88,7 +120,7 @@ export function videoTitleRotateRight() {
 }
 
 export function videoIdsAtIndex(i){ 
-  return videoIds[i]; 
+  return videoIdsInd[i]; 
 }
 
 export function videoIdRotateLeft(){ 
