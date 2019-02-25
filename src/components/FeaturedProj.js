0280584183863f4171294280c790cd68aa993bf3
Sean Nealon <signalflowsean@gmail.com>
2019-02-25 10:02:51 -0500

    moving towards react-anime library

--- src/components/FeaturedProj.js
+++ src/components/FeaturedProj.js
@@ -1,20 +1,33 @@
 import React from 'react'; 
 import '../styles/featured-proj.css'
 
-const FeaturedProj = () => (
-  <div className="wrap-collabsible one">
-    <input id="collapsible-1" className="toggle" type="checkbox"></input>
-    <label htmlFor="collapsible-1" className="lbl-toggle">Project 1</label>
-    <div className="collapsible-content">
-      <div className="content-inner">
-        <p>
-          This project does a lot of cool stuff and is really cool.
-          You should try it! It will be worth your time and it's fun
-          to play around with. Please now try...
-        </p>
-      </div>
-    </div>
-  </div>
-); 
+export default class FeaturedProj extends React.Component { 
+  constructor(props){ 
+    super(props); 
+    // this.domRef = React.createRef(); 
+  }
 
-export default FeaturedProj; 
+
+  render(){ 
+    return (
+      <div className={this.props.projNum + "wrap-collabsible"} >
+        <input id={this.props.projNum} className="toggle" type="checkbox"></input>
+        <label 
+          onClick={() => { 
+            this.props.clickedProj(); 
+          }}
+          htmlFor={this.props.projNum} 
+          className="lbl-toggle">Project 1</label>
+        <div className="collapsible-content">
+          <div className="content-inner">
+            <p>
+              This project does a lot of cool stuff and is really cool.
+              You should try it! It will be worth your time and it's fun
+              to play around with. Please now try...
+            </p>
+          </div>
+        </div>
+      </div>
+    ); 
+  }
+}
--- src/pages/FeaturedProjects.js
+++ src/pages/FeaturedProjects.js
@@ -1,16 +1,57 @@
 import React from 'react'; 
-// import {CollapsibleComponent, CollapsibleHead, CollapsibleContent} from 'react-collapsible-component'
 import { FeatureProj } from '../components/index.components'; 
+import Anime from 'react-anime'
 import featuredProjects from '../styles/index.styles'; // eslint-disable-line no-unused-vars
 
 
+export default class FeaturedProject extends React.Component{ 
+  constructor(props){ 
+    super(props); 
+    this.state = { 
+      projOneOpen : false, 
+      projTwoOpen : false, 
+      projThreeOpen: false
+    }; 
 
-const FeaturedProject = () => (
-  <section className="project-container">
-    <FeatureProj></FeatureProj>
-    <FeatureProj></FeatureProj>
-    <FeatureProj></FeatureProj>
-  </section>
-); 
+    // this.domRef = React.createRef(); 
+  }
 
-export default FeaturedProject; 
+  projOneClicked = () => { 
+    this.setState({projOneOpen : true}, () => { 
+      console.log('proj one was clicked'); 
+    })
+  }
+
+  projTwoCLlicked = () => {
+    this.setState({projTwoOpen: true}, () => { 
+    })
+  }
+
+  projThreeClick = () => { 
+    this.setState({projThreeOpen: true}, () => { 
+      console.log('proj three was clicked'); 
+    })
+  }
+
+  render(){
+    return ( 
+      <section className="featured-proj-container">
+        <FeatureProj 
+          clickedProj={() => this.projOneClicked()}
+          projNum='proj-1' 
+          className='one'>
+        </FeatureProj>
+        <FeatureProj 
+          clickedProj={() => this.projTwoCLlicked()}
+          projNum='proj-2' 
+          className='two'>
+        </FeatureProj>
+        <FeatureProj 
+          clickedProj={() => this.projThreeClick()}
+          projNum='proj-3' 
+          className='three'>
+        </FeatureProj>
+      </section>
+    ); 
+  }
+}; 
