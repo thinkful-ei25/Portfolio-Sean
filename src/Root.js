47f8e2f6272b490b53eee092ec57d9c7e6d9fbc2
Sean Nealon <signalflowsean@gmail.com>
2019-02-27 16:54:10 -0500

    did another update of resume

--- src/Root.js
+++ src/Root.js
@@ -10,7 +10,7 @@ export default class Root extends React.Component {
     return (
       <main>
     
-        <a className='resume' href='https://drive.google.com/file/d/1tHSsPUpBGX8pKGMn62ZAkn_1SglFa7E8/view?usp=sharing'
+        <a className='resume' href='https://drive.google.com/file/d/1-4Dg4CYN8vpfbWeXlUpabOu08t9_gK8_/view?usp=sharing'
         rel="noopener noreferrer" target='_blank'
         >  Resume
         </a>
